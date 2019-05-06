f5fffd39db7c7c87f295894b6525e86ae418649d
sparsematmul <44402616+sparsematmul@users.noreply.github.com>
2019-05-06 17:37:20 -0400

    Converted packet sizes to bits

--- go-src/experiment.py
+++ go-src/experiment.py
@@ -5,21 +5,28 @@ from itertools import *
 import random
 
 num_ingress = 3
-ISP_Cap = [2024,2024,2024]
-ISP_Queues = [80,80,80]
+ISP_Cap = 2024
+ISP_Queues = [100,100,100]
+
 Process_Cap = 1024
 Process_Queue = 100
+
 Server_Cap = 1024
 Backlog = 256
+
 Link_Cap = 1024
 Link_Queue = 100
+
 amp_factor = np.random.randint(8,13)
+print(amp_factor)
+
 num_rounds = 500
 
 Max_attack_budget = 700
-tcp_size = 0.000038
-udp_size = 0.0624
-dns_amp_size = amp_factor * udp_size
+
+tcp_size = 0.00004
+udp_size = 0.0625
+dns_size = amp_factor * udp_size
 
 RTT_QLearn = []
 RTT_randMix = []
@@ -31,99 +38,116 @@ def get_tuple(length, total):
 
 def attack(at_type,ing,pc,sc,lc,isp_cap):
     if(at_type is 'TCP'):
-        time.sleep(0.1)
+        #time.sleep(0.1)
         sc-= tcp_size
-        time.sleep(0.001)
+        #time.sleep(0.001)
         isp_cap[ing]-= tcp_size
     elif(at_type is 'UDP'):
-        time.sleep(0.01)
+        #time.sleep(0.01)
         lc-= udp_size
-        time.sleep(0.001)
+        #time.sleep(0.001)
         isp_cap[ing]-= udp_size
         pc-= udp_size
     else:
         for i in range(amp_factor):
-            time.sleep(0.01)
+            #time.sleep(0.01)
             lc-= udp_size
-            time.sleep(0.001)
+            #time.sleep(0.001)
             isp_cap[ing]-= udp_size
             pc-= udp_size
     return pc,sc,lc,isp_cap
 
 
-def randMixAttack(Process_Cap,Server_cap,Link_Cap,ISP_Cap,budget):
-    pc,sc,lc,isp_cap = Process_Cap,Server_cap,Link_Cap,ISP_Cap
+def randMixAttack(pc,sc,lc,ic,budget):
+    pc,sc,lc,isp_cap = pc,sc,lc,ic
     bits_to_attack = 0
+    
     attack_vol = {'TCP':0,'UDP':0,'DNS':0}
     while((attack_vol['TCP'] + attack_vol['UDP'] + attack_vol['DNS'])<=budget):
         type=np.random.randint(0,3)
+        
         if(type==0):
             pc,sc,lc,isp_cap=attack('TCP',1,pc,sc,lc,isp_cap)
             attack_vol['TCP']+=tcp_size
             bits_to_attack+=tcp_size
+            
         elif(type==1):
             pc,sc,lc,isp_cap=attack('UDP',1,pc,sc,lc,isp_cap)
             attack_vol['UDP']+=udp_size
             bits_to_attack+=udp_size
+            
         else:
             pc,sc,lc,isp_cap=attack('DNS',1,pc,sc,lc,isp_cap)
             attack_vol['DNS']+=dns_size
             bits_to_attack+=dns_size
-        if ((pc <= 0) or (sc <= 0) or (lc <= 0) or (isp_cap[0] <= 0) or (isp_cap[1] <= 0) or (isp_cap[2] <= 0)):
+            
+        if ((pc <= 0) and (sc <= 0) and (lc <= 0) and (isp_cap[0] <= 0) and (isp_cap[1] <= 0) and (isp_cap[2] <= 0)):
             return pc,sc,lc,isp_cap,bits_to_attack
         #    return 0,0,0,[0,0,0]
-        time.sleep(0.001)           #Congestion Delay
-    return pc,sc,lc,isp_cap,bits_to_attack
+        time.sleep(0.001)      
+        #Congestion Delay
+    return pc,sc,lc,isp_cap,budget
 
 def SmartAttack(vol,ing,pc,sc,lc,isp_cap):
     done = False
     bits_to_attack = 0
+    
     while(not done):
         type=np.random.randint(0,3)
+        
         if(vol[type]==0):
             pc,sc,lc,isp_cap=attack('TCP',ing,pc,sc,lc,isp_cap)
             vol[type]-=tcp_size
+            
         elif(vol[type]==1):
             pc,sc,lc,isp_cap=attack('UDP',ing,pc,sc,lc,isp_cap)
             vol[type]-=udp_size
+            
         else:
             pc,sc,lc,isp_cap=attack('DNS',ing,pc,sc,lc,isp_cap)
             vol[type]-=dns_size
+            
         bits_to_attack+= vol[type]
+        
         if(vol[type]<0):
             vol[type] = 0
         if(vol[0]+vol[1]+vol[2]==0):
             done = True
-        if ((pc <= 0) or (sc <= 0) or (lc <= 0) or (isp_cap[0] <= 0) or (isp_cap[1] <= 0) or (isp_cap[2] <= 0)):
+            
+        if ((pc <= 0) and (sc <= 0) and (lc <= 0) and (isp_cap[0] <= 0) and (isp_cap[1] <= 0) and (isp_cap[2] <= 0)):
             return pc,sc,lc,isp_cap,bits_to_attack
-        else:
-            pkts+=1
-    return pc,sc,lc,isp_cap,bits_to_attack
+
+    return pc,sc,lc,isp_cap,budget
                 
-def randIngressAttack(Process_Cap,Server_cap,Link_Cap,ISP_Cap,budget):
+def randIngressAttack(pc,sc,lc,ic,budget):
     bits_to_attack = 0
-    pc,sc,lc,isp_cap = Process_Cap,Server_cap,Link_Cap,ISP_Cap
+    pc,sc,lc,isp_cap = pc,sc,lc,ic
+    
     attack_vol = {'TCP':0,'UDP':0,'DNS':0}
     while((attack_vol['TCP'] + attack_vol['UDP'] + attack_vol['DNS'])<=budget):
         type=np.random.randint(0,3)
         randing=np.random.randint(0,num_ingress)
+        
         if(type==0):
             pc,sc,lc,isp_cap=attack('TCP',randing,pc,sc,lc,isp_cap)
             attack_vol['TCP']+=tcp_size
             bits_to_attack+=tcp_size
+            
         elif(type==1):
             pc,sc,lc,isp_cap=attack('UDP',randing,pc,sc,lc,isp_cap)
             attack_vol['UDP']+=udp_size
             bits_to_attack+=udp_size
+            
         else:
             pc,sc,lc,isp_cap=attack('DNS',randing,pc,sc,lc,isp_cap)
             attack_vol['DNS']+=dns_size
             bits_to_attack+=dns_size
+            
         time.sleep(0.001)           #Congestion Delay
-        if ((pc <= 0) or (sc <= 0) or (lc <= 0) or (isp_cap[0] <= 0) or (isp_cap[1] <= 0) or (isp_cap[2] <= 0)):
+        if ((pc <= 0) and (sc <= 0) and (lc <= 0) and (isp_cap[0] <= 0) and (isp_cap[1] <= 0) and (isp_cap[2] <= 0)):
             return pc,sc,lc,isp_cap,bits_to_attack
 #        print("Attack {}, PC {}, SC {}, LC {}, IC {}".format(i,pc,sc,lc,isp_cap))
-    return pc,sc,lc,isp_cap,bits_to_attack
+    return pc,sc,lc,isp_cap,budget
 
 def q_learn(RTT):
     states = 3 + num_ingress
@@ -195,72 +219,89 @@ if __name__ =="__main__":
     
     attack_budget = Max_attack_budget
     
-    pc,sc,lc,isp_cap = Process_Queue,Backlog,Link_Queue,ISP_Queue
+    pc,sc,lc,isp_cap = Process_Queue,Backlog,Link_Queue,ISP_Queues
     bits_Mix, bits_Ing, bits_Q = 0,0,0
     
-    start = time.time()
+    #start = time.time()
     for i in range(num_rounds):
-        budget = random.unifom(1, attack_budget)
+        
+        budget = random.uniform(1, attack_budget)
         attack_budget-= budget
+        
+        print("Budget in RandMix:",budget)
+        
         pc,sc,lc,isp_cap,bits=randMixAttack(pc,sc,lc,isp_cap,budget)
         bits_Mix+= bits
-        end = time.time()
-        if((pc <= 0) or (sc <= 0) or (lc <= 0) or (isp_cap[0] <= 0) or (isp_cap[1] <= 0) or (isp_cap[2] <= 0)):
+        #end = time.time()
+        if ((pc <= 0) and (sc <= 0) and (lc <= 0) and (isp_cap[0] <= 0) and (isp_cap[1] <= 0) and (isp_cap[2] <= 0)):
             RTT_randMix+= [10000]
             break
         else:
-            RTT_randMix+= [end - start]
+            congestion = (isp_cap[0]+isp_cap[1]+isp_cap[2])/ISP_Cap + pc/Process_Cap + sc/Server_Cap + lc/Link_Cap
+            RTT_randMix+= [congestion]
             
         if(attack_budget<=0):
             break
+        
         print("Attack {}, PC {}, SC {}, LC {}, IC {}".format(i+1,pc,sc,lc,isp_cap))
     print("Successful DDoS in Randmix in {} attacks by sending {} bits".format(i+1,bits_Mix))
         
     attack_budget = Max_attack_budget   
-    pc,sc,lc,isp_cap = Process_Queue,Backlog,Link_Queue,ISP_Queue   
-    start = time.time()
+    pc,sc,lc,isp_cap = Process_Queue,Backlog,Link_Queue,ISP_Queues   
+    #start = time.time()
     for i in range(num_rounds):
-        budget = random.unifom(1, attack_budget)
+        
+        budget = random.uniform(1, attack_budget)
+        print("Budget in Rand Ingress:",budget)
         attack_budget-= budget
+        
         pc,sc,lc,isp_cap,bits=randIngressAttack(pc,sc,lc,isp_cap,budget)
         bits_Ing+= bits
-        end = time.time()
-        if((pc <= 0) or (sc <= 0) or (lc <= 0) or (isp_cap[0] <= 0) or (isp_cap[1] <= 0) or (isp_cap[2] <= 0)):
+        #end = time.time()
+        if ((pc <= 0) and (sc <= 0) and (lc <= 0) and (isp_cap[0] <= 0) and (isp_cap[1] <= 0) and (isp_cap[2] <= 0)):
             RTT_randIngressAttack+= [10000]
             break
         else:
-            RTT_randIngressAttack+= [end - start]
+            congestion = (isp_cap[0]+isp_cap[1]+isp_cap[2])/ISP_Cap + pc/Process_Cap + sc/Server_Cap + lc/Link_Cap
+            RTT_randIngressAttack+= [congestion]
         
         if(attack_budget<=0):
             break
+        
         print("Attack {}, PC {}, SC {}, LC {}, IC {}".format(i+1,pc,sc,lc,isp_cap)) 
     print("Successful DDoS in RandIngress in {} attacks by sending {} packets".format(i+1,bits_Ing))
     
     attack_budget = Max_attack_budget 
-    pc,sc,lc,isp_cap = Process_Queue,Backlog,Link_Queue,ISP_Queue   
-    start = time.time()
+    pc,sc,lc,isp_cap = Process_Queue,Backlog,Link_Queue,ISP_Queues   
+    #start = time.time()
     RTT = [-10,-10,-10,10,10,20]
     
     for i in range(num_rounds):
-        attacker,ingress = q_learn(RTT)
-        attack_list =[attacker[0]*tcp_size,attacker[1]*udp_size,attacker[2]*dns_size]
         
+        attacker,ingress = q_learn(RTT)
+                
         budget = random.uniform(1,attack_budget)
         attack_budget-= budget
         
-        pc,sc,lc,isp_cap,bits=SmartAttack(attack_list,ingress,pc,sc,lc,isp_cap,budget)
+        print("Budget in QLearn:",budget)
+        
+        attack_list =[attacker[0]/100*budget,attacker[1]/100*budget,attacker[2]/100*budget]
+        
+        pc,sc,lc,isp_cap,bits=SmartAttack(attack_list,ingress,pc,sc,lc,isp_cap)
         bits_Q+= bits
-        end = time.time()
+        congestion = (isp_cap[0]+isp_cap[1]+isp_cap[2])/ISP_Cap + pc/Process_Cap + sc/Server_Cap + lc/Link_Cap
+        #end = time.time()
         for l in range(0,3):
-            RTT[l]-=(end-start)
+            RTT[l]-=(congestion)
         for l in range(3,6):
-            RTT[l]+=(end-start)
+            RTT[l]+=(congestion)
             
-        if((pc <= 0) or (sc <= 0) or (lc <= 0) or (isp_cap[0] <= 0) or (isp_cap[1] <= 0) or (isp_cap[2] <= 0)):
+        if ((pc <= 0) and (sc <= 0) and (lc <= 0) and (isp_cap[0] <= 0) and (isp_cap[1] <= 0) and (isp_cap[2] <= 0)):
             RTT_QLearn+= [10000]
             break
         else:
-            RTT_QLearn+= [end - start]
+            
+            RTT_QLearn+= [congestion]
         if(attack_budget<=0):
             break
         
